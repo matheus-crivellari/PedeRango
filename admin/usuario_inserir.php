1bf806fc67029fe391d9389897f73a65ac687708
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-03-02 19:33:49 -0300

    feat: filtro de cidade com ajax na tela de cadastro de usuario

--- admin/usuario_inserir.php
+++ admin/usuario_inserir.php
@@ -47,7 +47,7 @@ if($resultado){
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <style>
@@ -127,7 +127,7 @@ if($resultado){
                                 <div class="form-row">
                                     <div class="col-6">
                                         <label for="campoEstado">Estado</label>
-                                        <select class="form-control" name="estado" id="campoEstado">
+                                        <select onchange="mudaEstado()" class="form-control" name="estado" id="campoEstado">
                                             <?php foreach ($estados as $estado): ?>
                                                 <option value="<?php print $estado['id']?>"><?php print $estado['uf']?></option>
                                             <?php endforeach; ?>
@@ -135,7 +135,7 @@ if($resultado){
                                     </div>
                                     <div class="col-6">
                                         <label for="campoCidade">Cidade</label>
-                                        <select class="form-control" name="cidade" id="campoCidade">
+                                        <select disabled class="form-control" name="cidade" id="campoCidade">
                                             <option value="1">Piracicaba</option>
                                         </select>
                                     </div>
@@ -174,6 +174,49 @@ if($resultado){
             </div>
         </div>
     </div>
+    <script>
+        function mudaEstado() {
+            var cpEstado = $('#campoEstado');
+            var cpCidade = $('#campoCidade');
+
+            cpCidade.prop('disabled', true); // Desabilita o select de cidades
+            cpCidade.html('<option value="0">Carregando...</option>'); // Limpa o select de cidades
+
+            var idEstado = cpEstado.val();
+
+            var dados = {
+                estado_cod : idEstado
+            };
+
+            var url = 'http://localhost/pederango/webservice/get_cidades.php';
+
+            $.getJSON({
+                url: url,
+                data : dados
+            }).done(function (resposta) {
+                if(resposta != 'erro'){
+                    if(resposta.length){
+                        cpCidade.html(''); // Esvazia o select de cidades
+
+                        resposta.map(function (cidade) {
+                            cpCidade.append('<option value="' + cidade.id + '">' + cidade.nome + '</option>');
+                        });
+
+                        cpCidade.prop('disabled', false); // Habilita o select de cidades
+                    }
+                }else{
+                    alert('Houve um erro! Por favor tente novamnete mais tarde.');
+                    window.history.back();
+                }
+            }).fail(function () {
+                alert('Houve um erro! Por favor tente novamnete mais tarde.');
+                window.history.back();
+            });
+
+        }
+
+        mudaEstado();
+    </script>
 </body>
 
 </html>
