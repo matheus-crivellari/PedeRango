68cf8a7138b310e5664c516ec4b89b1e25441b7e
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-03-03 13:06:24 -0300

    feat: cadastro de produto

---
+++ admin/produto_inserir.php
@@ -0,0 +1,158 @@
+<?php
+// Inclui a conexao
+require_once '../conexao/conecta.php';
+
+// Obtem os dados do form
+$nome_prod       = $_POST['nome_prod']      ?? '';
+$resumo_prod     = $_POST['resumo_prod']    ?? '';
+$valor_prod      = $_POST['valor_prod']     ?? '';
+$descricao_prod  = $_POST['descricao_prod'] ?? '';
+$imagem_prod     = $_POST['imagem_prod']    ?? '';
+
+// Valida as vars do form
+if($nome_prod){
+
+    $sql = "INSERT INTO produtos_tb VALUES (0,'$nome_prod','$resumo_prod','$valor_prod','$descricao_prod','$imagem_prod')";
+
+    $resultado = mysqli_query($conexao, $sql);
+
+    if($resultado){
+        $_SESSION['msg'] = 'Produto cadastrado com sucesso.';
+        header('Location: produto_listar.php');
+        exit; // Necessario pq nossa página contem conteudo html além do header
+    }else{
+        $_SESSION['erro'] = 'Houve um erro ao alterar o registro.';
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Produtos</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <style>
+        html, body {
+            background-color: #dadada;
+            height: 100%;
+        }
+
+        #principal{
+            background-color: white;
+            height: 100%;
+        }
+
+        .pag{
+            pointer-events: none;
+            user-select: none;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div id="principal" class="container">
+        <div class="row">
+            <div class="col-12 p-0">
+                <?php require_once 'header.php' ?>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-12">
+
+                <div class="row">
+                    <div class="col-8 offset-2">
+                        <h1>Criar produto</h1>
+                        <hr>
+                        <?php if(isset($_SESSION['msg'])): ?>
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                <?php print $_SESSION['msg'] ?>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <?php unset($_SESSION['msg']); ?>
+                        <?php endif;?>
+
+                        <?php if(isset($_SESSION['erro'])): ?>
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <?php print $_SESSION['erro'] ?>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <?php unset($_SESSION['erro']); ?>
+                        <?php endif;?>
+                        <form method="post">
+                            <!-- Campo nome produto -->
+                            <div class="form-group">
+                                <label for="campoNomeProd">Produto</label>
+                                <input class="form-control" type="text" name="nome_prod" id="campoNomeProd" placeholder="Digite um nome para o produto">
+                                <small>Dê um nome para o seu produto.</small>
+                            </div>
+
+                            <!-- Campo resumo produto -->
+                            <div class="form-group">
+                                <label for="campoResumoProd">Resumo</label>
+                                <input class="form-control" type="text" name="resumo_prod" id="campoResumoProd" placeholder="Digite o resumo do produto">
+                                <small>Breve descrição para o seu produto.</small>
+                            </div>
+
+                            <!-- Campo endereço -->
+                            <div class="form-group">
+                                <label for="campoValor">Valor</label>
+                                <input class="form-control" type="text" name="valor_prod" id="campoValor" placeholder="999.99">
+                            </div>
+
+                            <!-- Campo descricao produto -->
+                            <div class="form-group">
+                                <label for="campoDescricaoProd">Descrição</label>
+                                <input class="form-control" type="text" name="descricao_prod" id="campoDescricaoProd" placeholder="Digite a descrição do produto">
+                                <small>Descrição completa para o seu produto.</small>
+                            </div>
+
+                            <!-- Campo senha -->
+                            <div class="form-group">
+                                <label for="campoSenha">Senha</label>
+                                <input class="form-control" type="text" name="senha" id="campoSenha" placeholder="Digite a senha do produto">
+                                <small>Até 8 dígitos.</small>
+                            </div>
+
+                            <!-- Campo tipo -->
+                            <div class="form-group">
+                                <label for="campoTipo">Tipo do produto</label>
+                                <select class="form-control" name="tipo" id="campoTipo">
+                                    <option value="com">Comum</option>
+                                    <option value="ent">Entregador</option>
+                                    <option value="adm">Administrador</option>
+                                </select>
+                            </div>
+
+                            <!-- Enviar / Cancelar -->
+                            <div class="form-row">
+                                <div class="col-6">
+                                    <button class="btn btn-block btn-primary" type="submit" value="Salvar"><i class="fa fa-floppy-o"></i> Salvar</button>
+                                </div>
+                                <div class="col-6">
+                                    <a href="produto_listar.php" class="btn btn-block btn-secondary"><i class="fa fa-arrow-left"></i> Voltar</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ admin/produto_listar.php
@@ -0,0 +1,158 @@
+<?php
+// Inclui a conexao
+require_once '../conexao/conecta.php';
+
+$resultado = mysqli_query($conexao, 'SELECT * FROM produtos_tb');
+if($resultado){
+    $total          = mysqli_num_rows($resultado);
+    $limit          = 5;
+    $pagina_total   = ceil($total / $limit);
+    $pagina_atual   = isset($_GET['pag']) ? $_GET['pag'] : 1;
+    $offset         = $limit * ($pagina_atual - 1);
+
+    $resultado = mysqli_query($conexao, "SELECT * FROM produtos_tb LIMIT $offset, $limit");
+    $produtos = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+}else{
+    $produtos = [];
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Produtos</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <style>
+        html,
+        body {
+            background-color: #dadada;
+            height: 100%;
+        }
+
+        #principal {
+            background-color: white;
+            height: 100%;
+        }
+
+        .pag {
+            pointer-events: none;
+            user-select: none;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="principal" class="container">
+        <div class="row">
+            <div class="col-12 p-0">
+                <?php require_once 'header.php' ?>
+
+                <div class="p-3">
+
+                    <div class="alert alert-secondary" role="alert">
+                        <h4 class="alert-heading">Administração de produtos</h4>
+                        <hr>
+                        <p class="mb-0">Aqui você pode administrar seus produtos.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <?php if(isset($_SESSION['msg'])): ?>
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <?php print $_SESSION['msg']; ?>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <?php unset($_SESSION['msg']); ?>
+                <?php endif;?>
+
+                <?php if(isset($_SESSION['erro'])): ?>
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <?php print $_SESSION['erro']; ?>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <?php unset($_SESSION['erro']); ?>
+                <?php endif;?>
+
+                <a class="btn btn-primary" href="produto_inserir.php" role="button"><i class="fa fa-plus"></i> Novo produto</a>
+
+                <div class="table-responsive mt-3">
+                    <table class="table">
+                        <thead>
+                            <th>#</th>
+                            <th>Imagem</th>
+                            <th>Produto</th>
+                            <th width="40%">Resumo</th>
+                            <th>Valor</th>
+                            <th>&nbsp;</th>
+                        </thead>
+                        <tbody>
+                            <?php foreach($produtos as $usuario): ?>
+                            <?php $usuario = (object) $usuario; ?>
+                            <tr>
+                                <td>#</td>
+                                <td>Imagem</td>
+                                <td>Produto</td>
+                                <td>Resumo</td>
+                                <td>Valor</td>
+                                <td>
+                                    <a href="usuario_alterar.php?id=<?php print $usuario->codigo_usuario ?>" class="btn btn-primary"><i class="fa fa-pencil"></i> Editar</a>
+                                    <a onclick="return excluir('<?php print $usuario->nome_completo ?>')" href="usuario_excluir.php?id=<?php print $usuario->codigo_usuario ?>" class="btn btn-danger"><i class="fa fa-trash"></i> Excluir</a>
+                                </td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="row justify-content-center">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <?php if($pagina_atual > 1): ?>
+                            <li class="page-item"><a class="page-link" href="?pag=1">Primeira</a></li>
+                            <li class="page-item"><a class="page-link" href="?pag=<?php print $pagina_atual - 1; ?>">Anterior</a></li>
+                            <?php endif;?>
+
+                            <?php if($pagina_total > 1): ?>
+                            <li class="page-item"><a class="pag page-link text-muted"><?php print str_pad($pagina_atual, 3, '0', STR_PAD_LEFT) . '/' . str_pad($pagina_total, 3, '0', STR_PAD_LEFT);  ?></a>
+                            </li>
+                            <?php endif;?>
+
+                            <?php if($pagina_atual < $pagina_total): ?>
+                            <li class="page-item"><a class="page-link" href="?pag=<?php print $pagina_atual + 1; ?>">Próximo</a></li>
+                            <li class="page-item"><a class="page-link" href="?pag=<?php print $pagina_total; ?>">Última</a></li>
+                            <?php endif;?>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        function excluir(nome) {
+            if (confirm('Tem certeza que deseja excluir "' + nome + '"?')) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    </script>
+</body>
+
+</html>
